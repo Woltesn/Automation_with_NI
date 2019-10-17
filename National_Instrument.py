252760c5956b1f5fcdb1d64b112d8a8d415f6bfb
dpe <woltesn@gmail.com>
2019-10-17 16:04:08 +0300

    analog output from 14 2

--- National_Instrument.py
+++ National_Instrument.py
@@ -14,6 +14,6 @@ while True:
                 while a>= 0.1:
                     a-=0.1
                     print(task.write(a))
-                    time.sleep(0.3)
+                    time.sleep(0.2)
         task.stop()
         time.sleep(0.5)
