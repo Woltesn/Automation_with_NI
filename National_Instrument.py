97e162300a7bc2516da0595d81fcad0ba440c204
dpe <woltesn@gmail.com>
2019-10-17 16:02:31 +0300

    Initial commit

---
+++ National_Instrument.py
@@ -0,0 +1,19 @@
+import nidaqmx
+from math import*
+import time
+while True:
+    with nidaqmx.Task() as task:
+        task.ao_channels.add_ao_voltage_chan('Dev1/ao0', 'mychannel', 0, 5)
+        a = 0
+        while True:
+            if a>=0:
+                while a <= 4.9:
+                    a += 0.1
+                    print(task.write(a))
+                    time.sleep(0.2)
+                while a>= 0.1:
+                    a-=0.1
+                    print(task.write(a))
+                    time.sleep(0.3)
+        task.stop()
+        time.sleep(0.5)
